98273450887d0de33e1d876d01447122fda9143f
shuyu6 <sygoh6@hotmail.com>
2017-10-26 22:43:07 +0100

    Update Address.java
    declare the object needed in the class

--- src/Address.java
+++ src/Address.java
@@ -1,3 +1,25 @@
 class Address{
-
+  String address1, address2, address3, city, postcode, country;
+  
+  public Address(String address1, String address2, String address3, String city, String state, String postcode, String country){
+    this.address1 = address1;
+    this.address2 = address2;
+    this.address3 = address3;
+    this.city = city;
+    this.postcode = postcode;
+    this.country = country;
+  }
+  
+  public void updateAddress(String address1, String address2, String address3, String city, String state, String postcode, String country){
+    this.address1 = address1;
+    this.address2 = address2;
+    this.address3 = address3;
+    this.city = city;
+    this.postcode = postcode;
+    this.country = country;
+  }
+  
+  public Address getAddress(){
+    return new Address(address1, address2, address3, city, postcode, country);
+  }
 }
