7d3d4ed1061e1bd5f94898af417d6b14a345d2ac
Tereza Pashinska <32960896+TerezaPashinska@users.noreply.github.com>
2017-10-27 18:29:57 +0100

    Update Sculpture.java

--- src/Sculpture.java
+++ src/Sculpture.java
@@ -1,144 +1,70 @@
-import java.time.Year;
-import java.util.Date;
+/**
+ * Class for creating a sculpture object.
+ * @author Tereza Pashinska
+ * version 1.0
+ * Since 27.10.2017
+ *
+ */
 import java.util.List;
-import java.util.Optional;
 
-public class Sculpture {
+public class Sculpture extends Artwork {
 
-	private String title;
-	private Optional<String> description;
-	private String photo;
-	private String creator;
-	private Year yearCreated;
-	private double reservedPrice;
-	private int bidsAllowed;
-	private Date dateAdded;
-	private Dimensions dimensions;
+	/**
+	 *The material of the sculpture 
+	 */
 	private String material;
+	/**
+	 *Additional photos added
+	 */
 	private List<String> photos;
 	
 	
-	public Sculpture(String title, Optional<String> description,String photo,String creator,Year yearCreated, double reservedPrice,
-			int bidsAllowed, Date dateAdded,Dimensions dimension, String material, List<String> photos){
-		this.title = title;
-		this.description = description;
-		this.photo = photo;
-		this.creator = creator;
-		this.yearCreated = yearCreated;
-		this.reservedPrice = reservedPrice;
-		this.bidsAllowed = bidsAllowed;
-		this.dateAdded = dateAdded;
-		this.dimensions = dimensions;
+	/**
+	 * Constructs an object of type Sculpture
+	 * @param material
+	 * 				The material of the sculpture.
+	 * @param photos
+	 * 				List of photos added
+	 */
+	public Sculpture (String material, List<String> photos){
+		super (title, description, reservePrice, maxBids, dimension,yearCreated);
 		this.material = material;
 		this.photos = photos;
 		
 	}
 	
-	
-	public String getTitle() {
-		return title;
-	}
 
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-
-	public Optional<String> getDescription() {
-		return description;
-	}
-
-
-	public void setDescription(Optional<String> description) {
-		this.description = description;
-	}
-
-
-	public String getPhoto() {
-		return photo;
-	}
-
-
-	public void setPhoto(String photo) {
-		this.photo = photo;
-	}
-
-
-	public String getCreator() {
-		return creator;
-	}
-
-
-	public void setCreator(String creator) {
-		this.creator = creator;
-	}
-
-
-	public Year getYearCreated() {
-		return yearCreated;
-	}
-
-
-	public void setYearCreated(Year yearCreated) {
-		this.yearCreated = yearCreated;
-	}
-
-
-	public double getReservedPrice() {
-		return reservedPrice;
-	}
-
-
-	public void setReservedPrice(double reservedPrice) {
-		this.reservedPrice = reservedPrice;
-	}
-
-
-	public int getBidsAllowed() {
-		return bidsAllowed;
-	}
-
-
-	public void setBidsAllowed(int bidsAllowed) {
-		this.bidsAllowed = bidsAllowed;
-	}
-
-
-	public Date getDateAdded() {
-		return dateAdded;
-	}
-
-
-	public void setDateAdded(Date dateAdded) {
-		this.dateAdded = dateAdded;
-	}
-
-
-	public Dimensions getDimensions() {
-		return dimensions;
-	}
-
-
-	public void setDimensions(Dimensions dimensions) {
-		this.dimensions = dimensions;
-	}
-
-
+	/**
+	 * Method getting the material of a sculpture
+	 * @return The material.
+	 */
 	public String getMaterial() {
 		return material;
 	}
 
 
+	/**
+	 * Resets the material.
+	 * @param The material of the sculpture.
+	 */
 	public void setMaterial(String material) {
 		this.material = material;
 	}
 
 
+	/**
+	 * Method getting the list of photos
+	 * @return List of photos
+	 */
 	public List<String> getPhotos() {
 		return photos;
 	}
 
+	/**
+	 * Method adding photos.
+	 * @param list of photos
+	 */
 	public void addPhoto(List<String>photos){
 		this.photos = photos;
 	}
