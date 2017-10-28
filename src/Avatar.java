5be3c656c02de318aeb483a42507bb1d9f07bd27
shuyu6 <sygoh6@hotmail.com>
2017-10-28 22:43:32 +0100

    update Avatar Class
    suggest: remove this class, replace Avatar class in User to String and name it as imageLocation

--- src/Avatar.java
+++ src/Avatar.java
@@ -1,3 +1,24 @@
-class Avatar{
+/**
+ *
+ * @author shuyu6
+ */
+public class Avatar {
+    String imageLocation;
 
+    public Avatar() {
+    }
+
+    public Avatar(String imageLocation) {
+        this.imageLocation = imageLocation;
+    }
+
+    public String getImageLocation() {
+        return imageLocation;
+    }
+
+    public void setImageLocation(String imageLocation) {
+        this.imageLocation = imageLocation;
+    }
+    
+        
 }
