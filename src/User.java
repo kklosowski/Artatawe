a01630efdbb294f2efbe589e31383d297cf1cc73
Marceli Wac <marceli@cardiocube.com>
2017-10-28 12:47:06 +0000

    tweaked the design and updated the way last login date is set

--- src/User.java
+++ src/User.java
@@ -1,27 +1,74 @@
 import java.sql.Timestamp;
+import java.util.Date;
 
+/**
+ * User is the class that stores user-related data and thier last
+ * login date.
+ * @author	Marceli Wac
+ */
 class User{
+
+	/**
+	 * user's first name
+	 */
 	private final String firstName;
+
+	/**
+	 * user's last name
+	 */
 	private final String lastName;
+
+    /**
+     * user's nickname (or username) also used as login credentials
+     */
 	private final String userName;
+
+    /**
+     * user's mobile number
+     */
 	private String mobileNo;
+
+    /**
+     * user's postal address
+     */
 	private Address address;
+
+    /**
+     * timestamp of user's last log-in
+     */
 	private Timestamp lastLoggedIn;
+
+    /**
+     * user's profile picture
+     */
 	private Avatar avatar;
 
+	/**
+	 * Class constructor specifying all of user information except
+	 * the last login date which is set automatically to the current
+	 * date.
+	 *
+	 * @param firstName		user's first name
+	 * @param lastName		user's last name
+	 * @param userName		user's nickname (or username)
+	 * @param mobileNo		user's mobile number
+	 * @param address		user's postal address
+	 * @param avatar		user's profile picture
+	 */
 	public User(String firstName,
 				String lastName,
 				String userName,
 				String mobileNo,
 				Address address,
 				Avatar avatar){
-//		Timestamp currentTime = new Timestamp();
+		Date date = new Date();
+		Timestamp currentTime = new Timestamp(date.getTime());
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.userName = userName;
 		this.mobileNo = mobileNo;
 		this.address = address;
-//		this.lastLoggedIn = currentTime.getTime();
+		this.lastLoggedIn = currentTime;
 		this.avatar = avatar;
 	}
 
@@ -51,5 +98,21 @@ class User{
 
 	public Avatar getAvatar(){
 		return this.avatar;
+	}
+
+	public void setMobileNo(String newMobileNo){
+		this.mobileNo = newMobileNo;
+	}
+
+	public void setAddress(Address newAddress){
+		this.address = newAddress;
+	}
+
+	public void setLastLoggedIn(Timestamp newLastLoggedIn){
+		this.lastLoggedIn = newLastLoggedIn;
+	}
+
+	public void setAvatar(Avatar newAvatar){
+		this.avatar = newAvatar;
 	}
 }
