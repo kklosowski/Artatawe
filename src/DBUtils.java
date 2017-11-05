6d048ff78f4470f48ec06625abb0b7e314874754
kklosowski <kamil.klosowski@yahoo.com>
2017-11-05 17:24:42 +0000

    DbUtils

--- src/DBUtils.java
+++ src/DBUtils.java
@@ -1,5 +1,28 @@
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBUtils {
+
+    public static List<User> constructUsersFromRS(ResultSet rs){
+        return new ArrayList<>();
+    }
+
+    public static List<Auction> constructAuctionsFromRS(ResultSet rs){
+        return new ArrayList<>();
+    }
+
+    public static List<Artwork> constructArtworksFromRS(ResultSet rs){
+        return new ArrayList<>();
+    }
+
+    public static List<Bid> constructBidsFromRS(ResultSet rs){
+        return new ArrayList<>();
+    }
+
+    public static List<Address> constructAddressesFromRS(ResultSet rs){
+        return new ArrayList<>();
+    }
+
 
 }
