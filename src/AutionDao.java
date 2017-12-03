584bbc8a4009b07db62293c74d803780f2db7c7e
shuyu6\shuyu6 <sygoh6@gmail.com>
2017-12-03 23:03:10 +0000

    comment some code buggy code in order to make the program runnable

--- src/AutionDao.java
+++ src/AutionDao.java
@@ -10,6 +10,7 @@ import java.util.List;
  */
 public class AutionDao {
     private final SQLiteSingleton connection = SQLiteSingleton.getConnection();
+    //TODO : error here
 
     public List<Auction> getAllAuctions() throws SQLException{
         List<Auction> auctions = new ArrayList<>();
@@ -23,35 +24,34 @@ public class AutionDao {
         return auctions;
     }
 
-    public Auction getAuction(int auctionId){
+    public Auction getAuction(int auctionId) throws SQLException{
         ResultSet auctionResultSet = connection.query(
                 String.format("SELECT * FROM Auction WHERE auction_id = %s", auctionId));
         return DBUtils.constructAuctionFromRS(auctionResultSet);
     }
 
-    public void updateAuction(Auction auction, int auctionId){
+    public void updateAuction(Auction auction, int auctionId) throws SQLException{
         connection.query(String.format("UPDATE Auction SET artwork_id = %1%s, user_id = %2%s, current_price = %3%s, reserve_price = %4%s, date_added = %5%s, max_bids = %6%s WHERE auction_id = %7%s",
                 auction.getArtwork().getArtworkId(),
-                auction.getUser().getUserId(),
+                1,
                 auction.getCurrentPrice(),
                 auction.getReservePrice(),
                 auction.getDateAdded(),
                 auction.getMaxBids(),
                 auction.getAuctionId()));
     }
-
-    public void insertAuction(Auction auction){
+    public void insertAuction(Auction auction) throws SQLException{
         connection.insert(String.format("INSERT INTO Auction (%1%s, %2%s, %3%s, %4%s, %5%s, %6%s, %7%s)",
                 auction.getAuctionId(),
                 auction.getArtwork().getArtworkId(),
-                auction.getUser().getUserId(),
+                1,
                 auction.getCurrentPrice(),
                 auction.getReservePrice(),
                 auction.getDateAdded(),
                 auction.getMaxBids()));
     }
 
-    public void deleteAuction(Auction auction){
+    public void deleteAuction(Auction auction) throws SQLException{
         connection.query(String.format("DELETE FROM Auction WHERE auction_id = %1%s",
                 auction.getAuctionId()));
     }
