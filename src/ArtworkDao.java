a9f1764a18dd1502d5829d54010fea1c964938e4
randompasser <921494@swansea.ac.uk>
2017-12-04 17:55:13 +0000

    ArtworkDao (WIP)

--- src/ArtworkDao.java
+++ src/ArtworkDao.java
@@ -1,22 +1,54 @@
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
- * @author  Kamil Klosowski
- * @since   1/12/2017
+ * @author  Kamil Klosowski, Michael Lam
+ * @since   4/12/2017
  */
 public class ArtworkDao {
-    public Artwork getArtwork(int artwork_id){
+    /**
+     * Get the connection form the class - sqlitegingleton.
+     * */
+    private final SQLiteSingleton connection = SQLiteSingleton.getConnection();
+    /**
+     * Address table name in database.
+     * */
+    private final String ADDRESS_TABLE_NAME = "artwork";
+
+
+    public Artwork getArtwork(int artwork_id) throws SQLException{
+        String searchArtworkQuery = String.format("SELECT * FROM %s WHERE artwork_id = %d", ADDRESS_TABLE_NAME, artwork_id);
+        ResultSet rs = connection.query(searchArtworkQuery);
+
+        if (rs.next()) {
+            return DBUtils.constructAddressFromRS(rs);
+        }
         return null;
     }
 
-    public int updateArtwork(Artwork artwork){
-        return 0;
+    public int updateArtwork(Artwork artwork) throws SQLException{
+        int artwork_id = artwork.getArtworkId();
+        String title = artwork.getTitle();
+        String description = artwork.getDescription();
+        String artist = artwork.getArtist();
+        String primaryPicture = artwork.getPrimaryPicture();
+        int yearCreated = artwork.getYearCreated();
+
+        String updateArtworkQuery = String.format("UPDATE %s SET artwork_id = %1%s, ");
+        connection.query(updateArtworkQuery);
     }
 
-    public int deleteArtwork(Artwork artwork){
-        return 0;
+    public int deleteArtwork(Artwork artwork) throws SQLException{
+        int artwork_id = artwork.getArtworkId();
+        String deleteArtworkQuery = String.format("DELETE * from %s WHERE artwork_id = %d", ADDRESS_TABLE_NAME, artwork_id);
+        connection.query(deleteArtworkQuery);
     }
 
-    public int insertArtwork(Artwork artwork){
-        return 0;
+    public int insertArtwork(Artwork artwork) throws SQLException{
+        return 0;   
     }
 }
