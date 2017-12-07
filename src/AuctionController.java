459ee6db2c87960e1f936735adb10cbc6d37ccc8
Marceli-Wac <marceliwac@gmail.com>
2017-12-07 23:24:01 +0000

    Added ErrorController for better crash handling

---
+++ src/AuctionController.java
@@ -0,0 +1,8 @@
+package controllers;
+
+import javafx.scene.layout.Pane;
+
+public class auctionController {
+    private Pane view;
+    private auctionModel model;
+}
---
+++ src/ErrorController.java
@@ -0,0 +1,41 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class ErrorController {
+    private Scene view;
+    private String DEFAULT_ERROR_MESSAGE = "Unknown error";
+    private String errorMessage;
+
+    @FXML
+    private Text errorTextTarget;
+
+    public ErrorController(){
+        this.loadView();
+        this.errorMessage = DEFAULT_ERROR_MESSAGE;
+        this.errorTextTarget.setText(errorMessage);
+    }
+
+    public ErrorController(String errorText){
+        this.loadView();
+        this.errorMessage = errorText;
+        this.errorTextTarget.setText(errorMessage);
+    }
+
+    public Scene getView(){
+        return this.view;
+    }
+
+    private void loadView(){
+        try {
+            Pane errorView = FXMLLoader.load(getClass().getResource("/views/error.fxml"));
+            this.view = new Scene(errorView);
+        }catch(Exception e){
+            System.out.println("Fatal error occurred while loading error message.");
+            System.out.println("Previous error message to display: '" + this.errorMessage + "'");
+        }
+    }
+}
