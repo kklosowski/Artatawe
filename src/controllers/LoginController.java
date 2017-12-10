233872b9fb971589b0975f22c378aa3d029a3a96
Marceli-Wac <marceliwac@gmail.com>
2017-12-10 19:19:34 +0000

    Started implementation of login controller

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,11 +1,33 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController extends ViewController {
 
     private final String CONTENT_FXML_URL = "/views/login.fxml";
 
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private Button register;
+
     public LoginController() {
         loadViewController(CONTENT_FXML_URL);
+    }
+
+    public void verifyUser(){
+
+    }
+
+    private boolean validateUser(String username){
+
+        return true;
     }
 
 }
