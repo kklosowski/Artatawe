55e6d66da01d6591f8b38447043c7c49e868622a
Tereza Pashinska <32960896+TerezaPashinska@users.noreply.github.com>
2017-12-11 02:31:09 +0000

    updated comments

--- src/artatawe/Painting.java
+++ src/artatawe/Painting.java
@@ -1,19 +1,41 @@
 package artatawe;
+
 /**
- * This class contains the data and methods for a painting
+ * Paiting is a class that contains all the actions needed for constructing a painting object.
+ *
+ * @author Morgan David
+ * @version 1.0
  */
+
 public class Painting extends Artwork {
-    /* Width of the painting */
+    
+    /*
+    * The width of the painting
+    */
     private double width;
-    /* Height of the paintng */
+    
+    /** 
+    * The height of the paintng
+    */
     private double height;
 
+    /**
+     * Constructs an object of type Painting.
+     * @param width The width of the painting.
+     * @param height The height of the painting.
+     */
     public Painting(String title, String description, String artist, String primaryPicture, int yearCreated, double width, double height) {
         super(title, description, artist, primaryPicture, yearCreated);
         this.width = width;
         this.height = height;
     }
 
+    /**
+     * Constructs an object of type Painting.
+     * @param artworkId The Id of the painting.
+     * @param width The width of the painting.
+     * @param height The height of the painting.
+     */
     public Painting(int artworkId, String title, String description, String artist, String primaryPicture, int yearCreated, double width, double height) {
         super(artworkId, title, description, artist, primaryPicture, yearCreated);
         this.width = width;
