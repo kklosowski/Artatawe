a395195507cba7126db65862ed167327a37df031
kklosowski <kamil.klosowski@yahoo.com>
2017-12-11 05:16:20 +0000

    Registration2

--- src/controllers/Register2Controller.java
+++ src/controllers/Register2Controller.java
@@ -55,7 +55,7 @@ public class Register2Controller {
         }
 
         ViewLoader l = new ViewLoader();
-        l.loadViewController(ViewLoader.LOGIN_URL);
+        l.loadViewController(ViewLoader.BROWSE_AUCTIONS_URL);
         Stage s = (Stage) rootPane.getScene().getWindow();
         s.setScene(l.getView());
     }
