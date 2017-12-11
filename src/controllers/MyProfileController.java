45075419fdbbfcdbbd62d2f20e28d2de70ca08b9
kklosowski <kamil.klosowski@yahoo.com>
2017-12-11 05:37:48 +0000

    User profile

--- src/controllers/MyProfileController.java
+++ src/controllers/MyProfileController.java
@@ -1,5 +1,34 @@
 package controllers;
 
+import artatawe.User;
+import dataAccessObjects.UserDao;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MyProfileController {
 
+    @FXML
+    private Label username;
+
+    @FXML
+    private Label firstName;
+
+    @FXML
+    private Label lastName;
+
+    @FXML
+    private Label mobile;
+
+    @FXML
+    private Label address;
+
+    @FXML
+    private Label postcode;
+
+    @FXML
+    public void initialize(){
+
+        User user = (User) SessionStorage.sessionData.get("loggedUser");
+
+    }
 }
