2fb5da747639a8580cdc3ad9b1cb4c30fab49e98
Marceli-Wac <marceliwac@gmail.com>
2017-12-11 08:09:06 +0000

    Add auction

--- src/controllers/AddAuctionController.java
+++ src/controllers/AddAuctionController.java
@@ -3,10 +3,17 @@ package controllers;
 import artatawe.Artwork;
 import artatawe.Auction;
 import artatawe.Sculpture;
+import artatawe.User;
+import dataAccessObjects.AuctionDao;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-import java.awt.*;
+import java.sql.SQLException;
+import java.util.Date;
 
 public class AddAuctionController{
 
@@ -29,17 +36,46 @@ public class AddAuctionController{
     private TextArea description;
 
     @FXML
+    TextField w,h,d;
+
+    @FXML
     private Button createButton;
 
 
     public void createAuction(){
-//        Artwork artwork = new Sculpture()
-//        Auction auction = new Auction(
-//                SessionStorage.sessionData.get("loggedUser"),
-//                0,
-//                Double.parseDouble(reservedPrice.getText()),
-//                Integer.parseInt(bidsAllowed.getText()),
-//        )
+        Artwork artwork = new Sculpture(
+                title.getText(),
+                description.getText(),
+                artist.getText(),
+                "",
+                1999,
+                "",
+                Double.parseDouble(w.getText()),
+                Double.parseDouble(h.getText()),
+                Double.parseDouble(d.getText()),
+                null
+        );
+        Auction auction = new Auction(
+                (User) SessionStorage.sessionData.get("loggedUser"),
+                0,
+                Double.parseDouble(reservedPrice.getText()),
+                Integer.parseInt(bidsAllowed.getText()),
+                artwork
+        );
+
+        AuctionDao auctionDao = new AuctionDao();
+        try {
+            auctionDao.insertAuction(auction);
+
+            ViewLoader l = new ViewLoader();
+            l.loadViewController(ViewLoader.BROWSE_AUCTIONS_URL);
+            Stage s = (Stage) creationDate.getScene().getWindow();
+            s.setScene(l.getView());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
