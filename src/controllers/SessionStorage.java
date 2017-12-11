3fc7a041d3d6230e553a8d599adea09671e10ec2
kklosowski <kamil.klosowski@yahoo.com>
2017-12-11 04:53:05 +0000

    Registration working

---
+++ src/controllers/SessionStorage.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import artatawe.User;
+
+/**
+ * @author Kamil Klosowski
+ * @project Artatawe
+ * @since 11/12/2017
+ */
+public class SessionStorage {
+    public static User loggedUser;
+}
